79581b4cc2d3a8c4da1eacdfbf7263ed2c2b145a
杨承翰 <ch-yang23@mails.tsinghua.edu.cn>
2024-11-20 20:58:39 +0800

    add particles

--- Simulation/src/BaseContainer.cpp
+++ Simulation/src/BaseContainer.cpp
@@ -61,46 +61,81 @@ BaseContainer::BaseContainer(SimConfig &config, bool GGUI)
   }
 
   object_initialize();
+
+  //========== Allocate memory ==========//
+  // Particle num of each grid
+  int num_grid = grid_num.prod(); // handle 3D together
+
+  particle_object_ids.resize(particle_max_num);
+  particle_positions.resize(particle_max_num, dim);
+  particle_velocities.resize(particle_max_num, dim);
+  particle_accelerations.resize(particle_max_num, dim);
+  particle_rest_volumes.resize(particle_max_num);
+  particle_masses.resize(particle_max_num);
+  particle_densities.resize(particle_max_num);
+  particle_pressures.resize(particle_max_num);
+  particle_materials.resize(particle_max_num);
+  particle_colors.resize(particle_max_num, 3);
+  particle_is_dynamic.resize(particle_max_num);
+
+  object_materials.resize(max_num_object);
+
+  object_num = num_fluid_object + num_rigid_object +
+               (add_domain_box ? 1 : 0); // add 1 for domain box object
+
+  rigid_particle_original_positions.resize(particle_max_num, dim);
+  rigid_body_is_dynamic.resize(max_num_object);
+  rigid_body_original_centers_of_mass.resize(max_num_object, dim);
+  rigid_body_masses.resize(max_num_object);
+  rigid_body_centers_of_mass.resize(max_num_object, dim);
+  rigid_body_rotations.resize(max_num_object, dim * dim);
+  rigid_body_torques.resize(max_num_object, dim);
+  rigid_body_forces.resize(max_num_object, dim);
+  rigid_body_velocities.resize(max_num_object, dim);
+  rigid_body_angular_velocities.resize(max_num_object, dim);
+  rigid_body_particle_num.resize(max_num_object);
+
+  // Buffer for sort
+  particle_object_ids_buffer.resize(particle_max_num);
+  particle_positions_buffer.resize(particle_max_num, dim);
+  rigid_particle_original_positions_buffer.resize(particle_max_num, dim);
+  particle_velocities_buffer.resize(particle_max_num, dim);
+  particle_rest_volumes_buffer.resize(particle_max_num);
+  particle_masses_buffer.resize(particle_max_num);
+  particle_densities_buffer.resize(particle_max_num);
+  particle_materials_buffer.resize(particle_max_num);
+  particle_colors_buffer.resize(particle_max_num, 3);
+  is_dynamic_buffer.resize(particle_max_num);
+
+  // Visibility of object
+  object_visibility.resize(max_num_object);
+
+  // Grid id for each particle
+  grid_ids.resize(particle_max_num);
+  grid_ids_buffer.resize(particle_max_num);
+  grid_ids_new.resize(particle_max_num);
+
+  if (GGUI) {
+    x_vis_buffer.resize(particle_max_num, dim);
+    color_vis_buffer.resize(particle_max_num, 3);
+  }
+
+  if (add_domain_box) {
+    // Add domain box
+    add_box(object_num - 1, domain_box_start, domain_box_size,
+            domain_box_thickness, material_rigid, false, particle_spacing,
+            Eigen::Vector3i(127, 127, 127));
+
+    object_visibility[object_num - 1] = 0;
+    object_materials[object_num - 1] = material_rigid;
+    rigid_body_is_dynamic[object_num - 1] = 0;
+    rigid_body_velocities.row(object_num - 1).setZero();
+    object_collection[object_num - 1] =
+        std::unordered_map<std::string, nlohmann::json>();
+  }
 }
 
 void BaseContainer::object_initialize() {
-  std::ifstream input("./data/models/dragon.obj");
-  Polyhedron poly;
-  if (input && (input >> poly)) {
-    rigid_bodies.push_back(poly);
-  } else {
-    std::cerr << "Error loading mesh from file." << std::endl;
-  }
-
-  // 使用indicators库显示进度条
-  indicators::ProgressBar bar{
-      indicators::option::BarWidth{50},
-      indicators::option::Start{"["},
-      indicators::option::Fill{"="},
-      indicators::option::Lead{">"},
-      indicators::option::Remainder{" "},
-      indicators::option::End{"]"},
-      indicators::option::PostfixText{"Loading rigid bodies"},
-      indicators::option::ForegroundColor{indicators::Color::green},
-      indicators::option::ShowElapsedTime{true},
-      indicators::option::ShowRemainingTime{true},
-      indicators::option::MaxProgress{100}};
-
-  for (int i = 0; i <= 100; ++i) {
-    bar.set_progress(i);
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
-  }
-
-  // 使用std::accumulate替代reduce
-  std::vector<int> numbers = {1, 2, 3, 4, 5};
-  int sum = std::accumulate(numbers.begin(), numbers.end(), 0);
-  std::cout << "Sum: " << sum << std::endl;
-
-  // 使用std::bind替代partial
-  auto add = [](int a, int b) { return a + b; };
-  auto add_five = std::bind(add, 5, std::placeholders::_1);
-  std::cout << "5 + 3 = " << add_five(3) << std::endl;
-
   //========== Compute number of particles ==========//
   fluid_particle_num = 0;
   rigid_body_particle_num = 0;
@@ -190,4 +225,187 @@ int BaseContainer::compute_box_particle_num(const Eigen::Vector3d &lower_corner,
   // Implement the function to compute the number of particles in a box
   // This is a placeholder implementation
   return 0;
+}
+
+void BaseContainer::insert_object() {
+  // Fluid block
+  for (auto &fluid : fluid_blocks) {
+    int obj_id = fluid["objectId"];
+
+    if (std::find(present_object.begin(), present_object.end(), obj_id) !=
+        present_object.end())
+      continue;
+    if (fluid["entryTime"] > total_time)
+      continue;
+
+    Eigen::Vector3d offset = fluid["translation"];
+    Eigen::Vector3d start = fluid["start"] + offset;
+    Eigen::Vector3d end = fluid["end"] + offset;
+    Eigen::Vector3d scale = fluid["scale"];
+    Eigen::Vector3d velocity = fluid["velocity"];
+    double density = fluid["density"];
+    Eigen::Vector3i color = fluid["color"];
+    object_id_fluid_body.insert(obj_id);
+
+    if (fluid.contains("visible")) {
+      object_visibility[obj_id] = fluid["visible"];
+    } else {
+      object_visibility[obj_id] = 1;
+    }
+
+    object_materials[obj_id] = material_fluid;
+    object_collection[obj_id] = fluid;
+
+    add_cube(obj_id, start, (end - start) * scale, velocity, density, true,
+             color, material_fluid, particle_spacing);
+
+    present_object.push_back(obj_id);
+  }
+
+  // Fluid body
+  for (auto &fluid_body : fluid_bodies) {
+    int obj_id = fluid_body["objectId"];
+
+    if (std::find(present_object.begin(), present_object.end(), obj_id) !=
+        present_object.end())
+      continue;
+    if (fluid_body["entryTime"] > total_time)
+      continue;
+
+    int num_particles_obj = fluid_body["particleNum"];
+    Eigen::MatrixXd voxelized_points_np = fluid_body["voxelizedPoints"];
+    Eigen::Vector3d velocity = fluid_body["velocity"];
+    double density = fluid_body["density"];
+    Eigen::Vector3i color = fluid_body["color"];
+
+    if (fluid_body.contains("visible")) {
+      object_visibility[obj_id] = fluid_body["visible"];
+    } else {
+      object_visibility[obj_id] = 1;
+    }
+
+    object_materials[obj_id] = material_fluid;
+    object_id_fluid_body.insert(obj_id);
+    object_collection[obj_id] = fluid_body;
+
+    add_particles(obj_id, num_particles_obj, voxelized_points_np,
+                  Eigen::MatrixXd::Constant(num_particles_obj, dim, velocity),
+                  Eigen::VectorXd::Constant(num_particles_obj, density),
+                  Eigen::VectorXd::Zero(num_particles_obj),
+                  Eigen::VectorXi::Constant(num_particles_obj, material_fluid),
+                  Eigen::VectorXi::Constant(num_particles_obj, 1),
+                  Eigen::MatrixXi::Constant(num_particles_obj, 3, color));
+
+    present_object.push_back(obj_id);
+    fluid_particle_num += num_particles_obj;
+  }
+
+  // Rigid body
+  for (auto &rigid_body : rigid_bodies) {
+    int obj_id = rigid_body["objectId"];
+
+    if (std::find(present_object.begin(), present_object.end(), obj_id) !=
+        present_object.end())
+      continue;
+    if (rigid_body["entryTime"] > total_time)
+      continue;
+
+    object_id_rigid_body.insert(obj_id);
+    int num_particles_obj = rigid_body["particleNum"];
+    rigid_body_particle_num[obj_id] = num_particles_obj;
+    Eigen::MatrixXd voxelized_points_np = rigid_body["voxelizedPoints"];
+    bool is_dynamic = rigid_body["isDynamic"];
+    Eigen::Vector3d velocity;
+    if (is_dynamic) {
+      velocity = rigid_body["velocity"];
+    } else {
+      velocity = Eigen::Vector3d::Zero(dim);
+    }
+    double density = rigid_body["density"];
+    Eigen::Vector3i color = rigid_body["color"];
+
+    if (rigid_body.contains("visible")) {
+      object_visibility[obj_id] = rigid_body["visible"];
+    } else {
+      object_visibility[obj_id] = 1;
+    }
+
+    object_materials[obj_id] = material_rigid;
+    object_collection[obj_id] = rigid_body;
+
+    add_particles(obj_id, num_particles_obj, voxelized_points_np,
+                  Eigen::MatrixXd::Constant(num_particles_obj, dim, velocity),
+                  Eigen::VectorXd::Constant(num_particles_obj, density),
+                  Eigen::VectorXd::Zero(num_particles_obj),
+                  Eigen::VectorXi::Constant(num_particles_obj, material_rigid),
+                  Eigen::VectorXi::Constant(num_particles_obj, is_dynamic),
+                  Eigen::MatrixXi::Constant(num_particles_obj, 3, color));
+
+    rigid_body_is_dynamic[obj_id] = is_dynamic;
+    rigid_body_velocities.row(obj_id) = velocity;
+
+    if (is_dynamic) {
+      rigid_body_masses[obj_id] = compute_rigid_body_mass(obj_id);
+      rigid_body_is_dynamic[obj_id] = 1;
+      // rigid_com = compute_rigid_body_center_of_mass(obj_id);
+      // ! here we assume the center of mass is exactly the base frame center
+      // and calculated it in the bullet solver.
+    }
+    present_object.push_back(obj_id);
+  }
+}
+
+void BaseContainer::add_cube(int object_id, const Eigen::Vector3d &lower_corner,
+                             const Eigen::Vector3d &cube_size,
+                             const Eigen::Vector3d &velocity, double density,
+                             bool is_dynamic, const Eigen::Vector3i &color,
+                             int material, double space) {
+  // Implement the function to add particles in a cube
+  // This is a placeholder implementation
+}
+
+void BaseContainer::add_particle(int p, int obj_id, const Eigen::Vector3d &x,
+                                 const Eigen::Vector3d &v, double density,
+                                 double pressure, int material, int is_dynamic,
+                                 const Eigen::Vector3i &color) {
+  particle_object_ids[p] = obj_id;
+  particle_positions.row(p) = x;
+  rigid_particle_original_positions.row(p) = x;
+  particle_velocities.row(p) = v;
+  particle_densities[p] = density;
+  particle_rest_volumes[p] = V0;
+  particle_masses[p] = V0 * density;
+  particle_pressures[p] = pressure;
+  particle_materials[p] = material;
+  particle_is_dynamic[p] = is_dynamic;
+  particle_colors.row(p) = color;
+}
+
+void BaseContainer::add_particles(
+    int object_id, int new_particles_num, const Eigen::MatrixXd &positions,
+    const Eigen::MatrixXd &velocities, const Eigen::VectorXd &densities,
+    const Eigen::VectorXd &pressures, const Eigen::VectorXi &materials,
+    const Eigen::VectorXi &is_dynamic, const Eigen::MatrixXi &colors) {
+  for (int p = particle_num; p < particle_num + new_particles_num; ++p) {
+    Eigen::Vector3d v = Eigen::Vector3d::Zero(dim);
+    Eigen::Vector3d x = Eigen::Vector3d::Zero(dim);
+    for (int d = 0; d < dim; ++d) {
+      v[d] = velocities(p - particle_num, d);
+      x[d] = positions(p - particle_num, d);
+    }
+    add_particle(p, object_id, x, v, densities[p - particle_num],
+                 pressures[p - particle_num], materials[p - particle_num],
+                 is_dynamic[p - particle_num], colors.row(p - particle_num));
+  }
+  particle_num += new_particles_num;
+}
+
+double BaseContainer::compute_rigid_body_mass(int object_id) {
+  double sum_m = 0.0;
+  for (int p_i = 0; p_i < particle_num; ++p_i) {
+    if (particle_object_ids[p_i] == object_id && particle_is_dynamic[p_i]) {
+      sum_m += particle_densities[p_i] * V0;
+    }
+  }
+  return sum_m;
 }
--- Simulation/src/BaseContainer.h
+++ Simulation/src/BaseContainer.h
@@ -1,7 +1,7 @@
 #ifndef BASE_CONTAINER_H
 #define BASE_CONTAINER_H
 
-#include "../utils/SimConfig.h"
+#include "SimConfig.h"
 #include <CGAL/IO/Polyhedron_iostream.h>
 #include <CGAL/Polyhedron_3.h>
 #include <CGAL/Simple_cartesian.h>
@@ -20,6 +20,28 @@ class BaseContainer {
 public:
   BaseContainer(SimConfig &config, bool GGUI = false);
 
+  void insert_object();
+  double compute_rigid_body_mass(int object_id);
+  void add_particle(int p, int obj_id, const Eigen::Vector3d &x,
+                    const Eigen::Vector3d &v, double density, double pressure,
+                    int material, int is_dynamic, const Eigen::Vector3i &color);
+  void add_particles(int object_id, int new_particles_num,
+                     const Eigen::MatrixXd &new_particles_positions,
+                     const Eigen::MatrixXd &new_particles_velocity,
+                     const Eigen::VectorXd &new_particle_density,
+                     const Eigen::VectorXd &new_particle_pressure,
+                     const Eigen::VectorXi &new_particles_material,
+                     const Eigen::VectorXi &new_particles_is_dynamic,
+                     const Eigen::MatrixXi &new_particles_color);
+  void _add_particles(int object_id, int new_particles_num,
+                      const Eigen::MatrixXd &new_particles_positions,
+                      const Eigen::MatrixXd &new_particles_velocity,
+                      const Eigen::VectorXd &new_particle_density,
+                      const Eigen::VectorXd &new_particle_pressure,
+                      const Eigen::VectorXi &new_particles_material,
+                      const Eigen::VectorXi &new_particles_is_dynamic,
+                      const Eigen::MatrixXi &new_particles_color);
+
 private:
   void object_initialize();
   Eigen::MatrixXd load_fluid_body(
@@ -33,6 +55,11 @@ private:
   int compute_box_particle_num(const Eigen::Vector3d &lower_corner,
                                const Eigen::Vector3d &cube_size, double space,
                                double thickness);
+  void add_cube(int object_id, const Eigen::Vector3d &lower_corner,
+                const Eigen::Vector3d &cube_size,
+                const Eigen::Vector3d &velocity, double density,
+                bool is_dynamic, const Eigen::Vector3i &color, int material,
+                double space);
 
   SimConfig &cfg;
   bool GGUI;
@@ -69,6 +96,57 @@ private:
   std::set<int> object_id_rigid_body;
   std::set<int> object_id_fluid_body;
   std::vector<int> present_object;
+
+  // Particle related properties
+  Eigen::VectorXi particle_object_ids;
+  Eigen::MatrixXd particle_positions;
+  Eigen::MatrixXd particle_velocities;
+  Eigen::MatrixXd particle_accelerations;
+  Eigen::VectorXd particle_rest_volumes;
+  Eigen::VectorXd particle_masses;
+  Eigen::VectorXd particle_densities;
+  Eigen::VectorXd particle_pressures;
+  Eigen::VectorXi particle_materials;
+  Eigen::MatrixXi particle_colors;
+  Eigen::VectorXi particle_is_dynamic;
+
+  Eigen::VectorXi object_materials;
+  int object_num;
+
+  Eigen::MatrixXd rigid_particle_original_positions;
+  Eigen::VectorXi rigid_body_is_dynamic;
+  Eigen::MatrixXd rigid_body_original_centers_of_mass;
+  Eigen::VectorXd rigid_body_masses;
+  Eigen::MatrixXd rigid_body_centers_of_mass;
+  Eigen::MatrixXd rigid_body_rotations;
+  Eigen::MatrixXd rigid_body_torques;
+  Eigen::MatrixXd rigid_body_forces;
+  Eigen::MatrixXd rigid_body_velocities;
+  Eigen::MatrixXd rigid_body_angular_velocities;
+  Eigen::VectorXi rigid_body_particle_num;
+
+  // Buffer for sort
+  Eigen::VectorXi particle_object_ids_buffer;
+  Eigen::MatrixXd particle_positions_buffer;
+  Eigen::MatrixXd rigid_particle_original_positions_buffer;
+  Eigen::MatrixXd particle_velocities_buffer;
+  Eigen::VectorXd particle_rest_volumes_buffer;
+  Eigen::VectorXd particle_masses_buffer;
+  Eigen::VectorXd particle_densities_buffer;
+  Eigen::VectorXi particle_materials_buffer;
+  Eigen::MatrixXi particle_colors_buffer;
+  Eigen::VectorXi is_dynamic_buffer;
+
+  // Visibility of object
+  Eigen::VectorXi object_visibility;
+
+  // Grid id for each particle
+  Eigen::VectorXi grid_ids;
+  Eigen::VectorXi grid_ids_buffer;
+  Eigen::VectorXi grid_ids_new;
+
+  Eigen::MatrixXd x_vis_buffer;
+  Eigen::MatrixXd color_vis_buffer;
 };
 
 #endif // BASE_CONTAINER_H
